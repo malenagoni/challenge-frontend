593d51ed1f3973d205ed7e7d7389de2d341a7a67
Malena <malennacg@gmail.com>
2021-07-20 12:51:21 -0300

    search feature on index done

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,25 +6,38 @@ import { fetchStudents, Student } from "../services/students";
 type Props = {};
 
 const Main: React.FC<Props> = ({}) => {
-    const onChangeHandler = (event: any) => {
-        // TODO
-    };
-
     const [students, setStudents] = useState<Student[]>([]);
+    const [input, setInput] = useState("");
+    const [fetch, setFetch] = useState(true);
 
     useEffect(() => {
         fetchStudents().then((data: Student[]) => setStudents(data));
-    }, []);
-    
+    }, [fetch]);
+
+    const onChangeHandler = (event: any) => {
+        let eventValue = event.target.value;
+        console.log(eventValue)
+        setInput(eventValue);
+        
+        const users: Student[] = students?.filter((s) => `${s?.first_name} ${s?.last_name}`.toLowerCase().includes(eventValue.toLowerCase()));
+        
+        if (eventValue === "") return setFetch((prevFetch) => !prevFetch);
+        return setStudents(users);
+    };
+
     return (
         <Box direction="column" pad="medium" height="100%" overflow="auto">
-            <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
+            <TextInput placeholder="type here" value={input} onChange={onChangeHandler} />
             <Box direction="row" wrap={true}>
-                {students.map((s,i) => (
-                    <Box margin="10px" key={i}>
-                        <UserCard user={s} />
-                    </Box>
-                ))}
+                {students.length > 0 ? (
+                    students?.map((s, i) => (
+                        <Box margin="10px" key={i}>
+                            <UserCard user={s} />
+                        </Box>
+                    ))
+                ) : (
+                    <div>No results</div>
+                )}
             </Box>
         </Box>
     );
